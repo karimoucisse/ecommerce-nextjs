08f27a8b274879d8bf145b3cdb5f7c7e9ed31092
karimou cisse <karimou@pylote.io>
2023-11-11 23:28:06 +0100

    add: image in puplic folder

--- components/PromoSection.tsx
+++ components/PromoSection.tsx
@@ -1,38 +1,36 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const PromoSection = () => {
   return (
-    <div className="relative overflow-hidden bg-white">
-      <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
-        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-          <div className="sm:max-w-lg">
-            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Explorez la mode à chaque pas.
-            </h1>
-            <p className="mt-4 text-xl text-gray-500">
-              votre destination ultime pour des sneakers, des chaussures et des
-              vêtements à la pointe du style. Découvrez une collection
-              soigneusement sélectionnée qui redéfinit l'élégance décontractée.
-              {/* Prêt(e) à affirmer votre style ? Parcourez notre sélection dès
-              maintenant */}
-            </p>
-          </div>
-
-          <div>
-            <div className="flex gap-4">
-              <button className="inline-block rounded-md border bg-blue px-12 py-3 text-center font-medium text-gray">
-                <Link href="/men">Homme</Link>
-              </button>
-              <button className="inline-block rounded-md border bg-blue px-12 py-3 text-center font-medium text-gray">
-                <Link href="/women">Femme</Link>
-              </button>
-              <button className="inline-block rounded-md border bg-blue px-12 py-3 text-center font-medium text-gray">
-                <Link href="/children">Enfant</Link>
-              </button>
-            </div>
-          </div>
-        </div>
+    <div className="relative py-[3rem] sm:px-6 lg:px-8 overflow-hidden    ">
+      <div className="sm:max-w-lg">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Explorez la mode à chaque pas.
+        </h1>
+        <p className="my-2 text-xl text-gray-500">
+          votre destination ultime pour des sneakers, des chaussures et des
+          vêtements à la pointe du style.
+        </p>
+      </div>
+      <Image
+        src="/fashion.jpg"
+        alt="Picture of the author"
+        width={600}
+        height={600}
+        className="right-0 top-0 md:absolute"
+      />
+      <div className="flex gap-4 mt-8 justify-center">
+        <button className="inline-block rounded-md border bg-blue px-8 py-3 text-center font-medium text-white">
+          <Link href="/men">Homme</Link>
+        </button>
+        <button className="inline-block rounded-md border bg-blue px-8 py-3 text-center font-medium text-white">
+          <Link href="/women">Femme</Link>
+        </button>
+        <button className="inline-block rounded-md border bg-blue px-8 py-3 text-center font-medium text-white">
+          <Link href="/children">Enfant</Link>
+        </button>
       </div>
     </div>
   );
