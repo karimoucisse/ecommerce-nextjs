6390aab459035c4743183a762a1f70274d335d8a
karimou cisse <karimou@pylote.io>
2023-11-12 00:22:56 +0100

    change: hero section

--- components/PromoSection.tsx
+++ components/PromoSection.tsx
@@ -4,14 +4,15 @@ import React from "react";
 
 const PromoSection = () => {
   return (
-    <div className="relative py-[3rem] md:px-4 lg:px-8 overflow-hidde lg:bg-red-400">
+    <div className="relative py-[3rem] md:px-4 lg:px-12 lg:py-[80px] overflow-hidden">
       <div className="sm:max-w-lg px-2">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
           Explorez la mode à chaque pas.
         </h1>
         <p className="mt-2 text-xl text-gray-500 md:mt-6 md:mb-10">
-          votre destination ultime pour des sneakers, des chaussures et des
-          vêtements à la pointe du style.
+          votre destination ultime pour des sneakers et des vêtements à la
+          pointe du style.Découvrez une collection soigneusement sélectionnée
+          qui redéfinit l'élégance décontractée.
         </p>
       </div>
       <Image
@@ -19,16 +20,16 @@ const PromoSection = () => {
         alt="Picture of the author"
         width={700}
         height={600}
-        className="right-0 top-0 rounded-md my-4  w-full md:absolute md:w-[500px]"
+        className="right-0 top-0 rounded-md my-4 w-full lg:absolute lg:w-[700px] -z-10"
       />
-      <div className="flex gap-x-4 px-1 justify-center md:justify-start">
-        <button className="inline-block rounded-md border bg-blue px-7 py-3 text-center font-medium text-white">
+      <div className="absolute lg:relative bottom-[100px] lg:bottom-0 flex gap-4 md:gap-12 px-1 w-full justify-center lg:justify-start">
+        <button className="inline-block rounded-md bg-gray lg:bg-blue px-7 py-3 text-center font-medium text-blue lg:text-white">
           <Link href="/men">Homme</Link>
         </button>
-        <button className="inline-block rounded-md border bg-blue px-7 py-3 text-center font-medium text-white">
+        <button className="inline-block rounded-md bg-gray lg:bg-blue px-7 py-3 text-center font-medium text-blue lg:text-white">
           <Link href="/women">Femme</Link>
         </button>
-        <button className="inline-block rounded-md border bg-blue px-7 py-3 text-center font-medium text-white">
+        <button className="inline-block rounded-md bg-gray lg:bg-blue px-7 py-3 text-center font-medium text-blue lg:text-white">
           <Link href="/children">Enfant</Link>
         </button>
       </div>
