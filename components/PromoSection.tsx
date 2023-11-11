ea984445397d81d2ce822ef1749703cab73f1bde
karimou cisse <karimou@pylote.io>
2023-11-11 23:56:42 +0100

    change: hero section

--- components/PromoSection.tsx
+++ components/PromoSection.tsx
@@ -4,12 +4,12 @@ import React from "react";
 
 const PromoSection = () => {
   return (
-    <div className="relative py-[3rem] sm:px-6 lg:px-8 overflow-hidden    ">
-      <div className="sm:max-w-lg">
+    <div className="relative py-[3rem] md:px-4 lg:px-8 overflow-hidde lg:bg-red-400">
+      <div className="sm:max-w-lg px-2">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
           Explorez la mode à chaque pas.
         </h1>
-        <p className="mt-2 text-xl text-gray-500">
+        <p className="mt-2 text-xl text-gray-500 md:mt-6 md:mb-10">
           votre destination ultime pour des sneakers, des chaussures et des
           vêtements à la pointe du style.
         </p>
@@ -17,11 +17,11 @@ const PromoSection = () => {
       <Image
         src="/fashion.jpg"
         alt="Picture of the author"
-        width={600}
+        width={700}
         height={600}
-        className="right-0 top-0 rounded-md my-4 md:absolute "
+        className="right-0 top-0 rounded-md my-4  w-full md:absolute md:w-[500px]"
       />
-      <div className="flex gap-x-2 px-1 justify-between md:justify-center">
+      <div className="flex gap-x-4 px-1 justify-center md:justify-start">
         <button className="inline-block rounded-md border bg-blue px-7 py-3 text-center font-medium text-white">
           <Link href="/men">Homme</Link>
         </button>
