4ac3995aeff9fafbb4c10389966ac3adb32c6525
karimou cisse <karimou@pylote.io>
2023-11-13 23:29:21 +0100

    add: product section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+import ProductsRow from "@/components/ProductsRow";
 import PromoSection from "@/components/PromoSection";
 
 export default function Home() {
+  const data = [
+    {
+      name: "Air force 1",
+      image: "/imageTest.jpg",
+    },
+    {
+      name: "Air force 1",
+      image: "/imageTest.jpg",
+    },
+    {
+      name: "Air force 1",
+      image: "/imageTest.jpg",
+    },
+    {
+      name: "Air force 1",
+      image: "/imageTest.jpg",
+    },
+    {
+      name: "Air force 1",
+      image: "/imageTest.jpg",
+    },
+  ] as const;
   return (
     <main>
       <PromoSection />
+      <ProductsRow title="Nouveautés" data={data} />
     </main>
   );
 }
---
+++ components/ProductsRow.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const ProductsRow = ({ title, data }) => {
+  return <section></section>;
+};
+
+export default ProductsRow;
