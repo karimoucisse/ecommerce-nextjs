000c4f1c10e0b038adc9f3290b050b828fe4babc
karimou cisse <karimou@pylote.io>
2023-11-14 23:09:17 +0100

    change: theme folder place, add: folder components, add: files header and footer in components folder

--- src/app/theme/globalTheme.ts
+++
@@ -1,8 +0,0 @@
-import { createTheme } from "@mui/material/styles";
-import { palette } from "./paletteTheme";
-
-export const theme = createTheme({
-  palette: {
-    ...palette,
-  },
-});
--- src/app/theme/paletteTheme.ts
+++
@@ -1,6 +0,0 @@
-export const palette = {
-  mode: "light",
-  primary: {
-    main: "#1c2930",
-  },
-};
